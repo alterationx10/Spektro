a6883dcc760286789dfcf5b7ed30b0f6d0d958cd
Mark Rudolph <mark@markslaboratory.com>
2011-03-14 12:00:03 -0400

    Updated calibration for main line and added output for second line

--- spektro.f90
+++ spektro.f90
@@ -1,7 +1,7 @@
         program spektro
         implicit none
-        double precision :: average(740,1), dummy, single, rgb(121360,1)
-        double precision :: x, y
+        double precision :: main_average(740,1), dummy, single, rgb(121360,1)
+        double precision :: x, y, second_average(740,1)
         integer :: i, j, narg, n_samples, y_int
         character(LEN=50) :: filename, outfile, infile
         character(LEN=3) :: char_n_samples, char_i
@@ -10,7 +10,8 @@
         narg = iargc()
 
         !Zero averge
-        average=0.0d0
+        main_average=0.0d0
+        second_average=0.0d0
 
         !Fix this later
         n_samples=3
@@ -50,30 +51,26 @@
 
         !Get the value we need from the array
         do j = 1, 740
-        !What Y value do we need?
-        y = -( 0.162189d0*DBLE(j) - 148.613d0 )
+        !What Y value do we need for the main line?
+        y = -( 0.151967d0*DBLE(j) - 142.573d0 )
         y_int = IDINT(y)
-        average(j,1) = average(j,1) + rgb(y_int*740+j,1)
-        !Crazy numbers seemed to come from average not being initialized
-        !to Zero...
-        !if ( ISNAN(rgb(y_int*740+j,1)) ) then
-        !write (*,*) "NaN line " , y_int*740+j, "file ", infile
-        !end if
-        !if ( rgb(y_int*740+j,1) > 100) then
-        !write (*,*) ">100 line " , y_int*740+j, "file ", infile
-        end if
-
-
-
+        main_average(j,1) = main_average(j,1) + rgb(y_int*740+j,1)
+        !What Y value do we need for the second line?
+        y = -( 0.159403d0*DBLE(j) - 134.966d0 )
+        y_int = IDINT(y)
+        second_average(j,1) = second_average(j,1) + rgb(y_int*740+j,1)
         end do !j
 
         end do !i
 
         ! Lets put everything back into a file
-        open(21,file="average.dat",STATUS="UNKNOWN")
+        open(21,file="main_average.dat",STATUS="UNKNOWN")
+        open(22,file="second_average.dat",STATUS="UNKNOWN")
         do i = 1, 740
-        write (21,*) i, average(i,1)/n_samples
+        write (21,*) i, main_average(i,1)/n_samples
+        write (22,*) i, second_average(i,1)/n_samples
         end do
         close(21)
+        close(22)
 
         end program
