7376606a35c629d97ec11e73dda89407b10df523
Mark Rudolph <mark@markslaboratory.com>
2011-03-23 02:33:06 -0400

    Seems to be going in th right direction...

--- spektro.cpp
+++ spektro.cpp
@@ -14,6 +14,12 @@
 using namespace std;
 #include <string>
 
+int main_line (int x) {
+	int y;
+	y = (0.161597*x - 592.69)*-1;
+	return(y);
+}
+
 
 int main(int argc, char *argv[]) {
 
@@ -27,6 +33,19 @@ int main(int argc, char *argv[]) {
 	int CAMERA_DEVICE_NUMBER = 0; // /dev/video0
 	int CAMERA_WIDTH_RESOLUTION = 1280;
 	int CAMERA_HEIGHT_RESOLUTION = 800;
+	int RECORD_MOVIE = 0; // 0/1 False/True
+	double AVERAGE[770];
+	double AVERAGE_B[770];
+	double AVERAGE_G[770];
+	double AVERAGE_R[770];
+
+	// Zero the AVERAGE array
+	for (int i=0; i<771; i++) {
+		AVERAGE[i] = 0;
+		AVERAGE_B[i] = 0;
+		AVERAGE_G[i] = 0;
+		AVERAGE_R[i] = 0;
+	}
 
 	// Can't seem to supress the error message, so tell
 	// the user it's safe to ignore...
@@ -50,31 +69,31 @@ int main(int argc, char *argv[]) {
 	// Will likely need to adjust resolution... Add later.
 
 	// Record a sample
-	cvGrabFrame(CAMERA);
-	SAMPLE = cvRetrieveFrame(CAMERA);
+	//cvGrabFrame(CAMERA);
+	//SAMPLE = cvRetrieveFrame(CAMERA);
 
 	// Lets see the image
-	cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE);
-	cvMoveWindow("mainWin", 100, 100);
-	cvShowImage("mainWin", SAMPLE);
-	cvWaitKey(0);
+	//cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE);
+	//cvMoveWindow("mainWin", 100, 100);
+	//cvShowImage("mainWin", SAMPLE);
+	//cvWaitKey(0);
 
 	// Lets save an image
-	cvSaveImage("test.bmp",SAMPLE);
+	//cvSaveImage("test.bmp",SAMPLE);
 
 	// Convert it to Grey ; will use this later
-	IplImage* GREY_SAMPLE = cvCreateImage(cvSize(FRAME_WIDTH,FRAME_HEIGHT),IPL_DEPTH_8U,1);
-	cvCvtColor(SAMPLE,GREY_SAMPLE,CV_RGB2GRAY);
+	//IplImage* GREY_SAMPLE = cvCreateImage(cvSize(FRAME_WIDTH,FRAME_HEIGHT),IPL_DEPTH_8U,1);
+	//cvCvtColor(SAMPLE,GREY_SAMPLE,CV_RGB2GRAY);
 
 	// Lets see the image
-	cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE);
-	cvMoveWindow("mainWin", 100, 100);
-	cvShowImage("mainWin", GREY_SAMPLE);
-	cvWaitKey(0);
+	//cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE);
+	//cvMoveWindow("mainWin", 100, 100);
+	//cvShowImage("mainWin", GREY_SAMPLE);
+	//cvWaitKey(0);
 
 	///////////////////////////////////////
 	// Lets trying recording a movie just for fun...
-	// It might be advantagious to record a movie and use hat as the "samples"...
+	if (RECORD_MOVIE) {
      	int isColor = 1;
 	int fps = 30;
 	int frameW = 1280;
@@ -92,20 +111,61 @@ int main(int argc, char *argv[]) {
 	}
 	// Release the writer
 	cvReleaseVideoWriter(&writer);
+	}
 	/////////////////////////////////
 	  
 	// Draw our calibration line to make sure its right
-	cvLine(SAMPLE, cvPoint(357,535), cvPoint(883,450), cvScalar(0,255,0), 1);
+	//cvLine(SAMPLE, cvPoint(357,535), cvPoint(883,450), cvScalar(0,255,0), 1); //Main line
+	//cvLine(SAMPLE, cvPoint(420,515), cvPoint(627,477), cvScalar(0,255,0), 1); //Second line
 	//Add in our equation later...
 
 	// Lets see the image
-	cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE);
-	cvMoveWindow("mainWin", 100, 100);
-	cvShowImage("mainWin", SAMPLE);
-	cvWaitKey(0);
+	//cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE);
+	//cvMoveWindow("mainWin", 100, 100);
+	//cvShowImage("mainWin", SAMPLE);
+	//cvWaitKey(0);
 
 	// Lets save an image
-	cvSaveImage("test-line.bmp",SAMPLE);
+	//cvSaveImage("test-line.bmp",GREY_SAMPLE);
+
+	//Try spitting out the "RGB" value
+	//CvScalar s;
+	//cout << s.val[0] << "\n";
+
+	// Lets try doing a loop
+	//IplImage* GREY_SAMPLE = cvCreateImage(cvSize(FRAME_WIDTH,FRAME_HEIGHT),IPL_DEPTH_8U,1);
+	CvScalar pixel;
+	int NSAMPLES = 1000;
+  	for (int i=0; i<NSAMPLES; i++) {
+  		cvGrabFrame(CAMERA);
+  		SAMPLE=cvRetrieveFrame(CAMERA);
+		//cvCvtColor(SAMPLE,GREY_SAMPLE,CV_RGB2GRAY);
+		// We want to pick x,y values for from 230 to 1000 
+		for (int x=230; x <1001; x++) {
+			// What y value do we need?
+			int y;
+			y = main_line(x);
+			// Get the pixel vale
+			pixel=cvGet2D(SAMPLE,y,x);
+			// Sum it up in an array
+			AVERAGE_B[x-230] = AVERAGE_B[x-230] + pixel.val[0];
+			AVERAGE_G[x-230] = AVERAGE_G[x-230] + pixel.val[1];
+			AVERAGE_R[x-230] = AVERAGE_R[x-230] + pixel.val[2];
+			AVERAGE[x-230] = AVERAGE[x-230] + AVERAGE_B[x-230] + AVERAGE_G[x-230] + AVERAGE_R[x-230];
+			// Move on to next image :-)
+		}
+  	}
+	// Divide by number of samples for averaging
+  	for (int i=0; i<771; i++) {
+		AVERAGE_B[i] = AVERAGE_B[i] / NSAMPLES ;
+		AVERAGE_G[i] = AVERAGE_G[i] / NSAMPLES ;
+		AVERAGE_R[i] = AVERAGE_R[i] / NSAMPLES ;
+		AVERAGE[i] = AVERAGE[i] / NSAMPLES ;
+		// spit it out while we're at it
+		//cout << i << " " << AVERAGE_B[i] << " " << AVERAGE_G[i] << " " << AVERAGE_R[i] << "\n";
+		cout << i << " " << AVERAGE[i] << "\n";
+	}
+
 
 	// Turn the camera off
 	cvReleaseCapture(&CAMERA);
